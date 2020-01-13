fcdbba5aebffa130f9d6189e064adb2ade4e4b60
Patrick Arbeiter <patrick.arbeiter@leftshift.one>
2020-01-13 10:29:45 +0100

    simplified leaflet import

--- src/lib/renderable/map/osm/index.ts
+++ src/lib/renderable/map/osm/index.ts
@@ -2,15 +2,14 @@ import {ISpecification} from "../../../api";
 import {INode} from "../../../support/node";
 import {AbstractMap} from "../AbstractMap";
 import {IMarker} from "../IMarker";
-import {Icon, icon, LatLngLiteral, Map as OSM, map, Marker, marker, tileLayer} from "leaflet";
-import "leaflet/dist/leaflet.css";
+import * as L from "leaflet";
 import Properties from "../../Properties";
 
 export class OpenStreetMap extends AbstractMap {
 
-    private markerIcon?: Icon;
-    private selectedMarkerIcon?: Icon;
-    private markers: Array<Marker> = [];
+    private markerIcon?: L.Icon;
+    private selectedMarkerIcon?: L.Icon;
+    private markers: Array<L.Marker> = [];
 
     readonly maxZoom = 18;
     readonly minZoom = 0;
@@ -29,24 +28,24 @@ export class OpenStreetMap extends AbstractMap {
 
     initMap(wrapper: INode) {
         const mapContainer = wrapper.find(".lto-map-container");
-        return map(mapContainer.unwrap(), {minZoom: this.minZoom, maxZoom: this.maxZoom})
-            .addLayer(tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}))
+        return L.map(mapContainer.unwrap(), {minZoom: this.minZoom, maxZoom: this.maxZoom})
+            .addLayer(L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}))
             .setView(this.getCenter(), this.getZoom());
     }
 
-    initMarkers(wrapper: INode, map: OSM) {
+    initMarkers(wrapper: INode, map: L.Map) {
         if (!this.spec.src) return;
         this.initMarkerIcons();
 
         let countSelections = 0;
         const maxSelections = this.spec.maxSelections || 1;
-        let activeMarker: Marker;
+        let activeMarker: L.Marker;
 
         OpenStreetMap.getMarkersFromSrc(this.spec.src).then((markers: Array<IMarker> | null) => {
             if (!markers) return;
 
             markers.forEach((m: IMarker) => {
-                const current = marker(m.position).addTo(map);
+                const current = L.marker(m.position).addTo(map);
 
                 current.getElement()!.setAttribute("data-label", m.label || "");
                 current.getElement()!.setAttribute("data-meta", JSON.stringify(m.meta) || "");
@@ -78,12 +77,12 @@ export class OpenStreetMap extends AbstractMap {
         });
     }
 
-    deactivateMarker(marker: Marker) {
+    deactivateMarker(marker: L.Marker) {
         marker.getElement()!.setAttribute("data-active", JSON.stringify(false));
         marker.setIcon(this.markerIcon!);
     }
 
-    activateMarker(marker: Marker) {
+    activateMarker(marker: L.Marker) {
         marker.getElement()!.setAttribute("data-active", JSON.stringify(true));
         marker.setIcon(this.selectedMarkerIcon!);
     }
@@ -96,12 +95,12 @@ export class OpenStreetMap extends AbstractMap {
             [Properties.resolve("OSM_SELECTED_MARKER_WIDTH") as number || 32,
                 Properties.resolve("OSM_SELECTED_MARKER_HEIGHT") as number || 32];
 
-        this.markerIcon = icon({iconUrl: this.getMarkerIconSrc(), iconSize: markerSize});
-        this.selectedMarkerIcon = icon({iconUrl: this.getSelectedMarkerIconSrc(), iconSize: selectedMarkerSize});
+        this.markerIcon = L.icon({iconUrl: this.getMarkerIconSrc(), iconSize: markerSize});
+        this.selectedMarkerIcon = L.icon({iconUrl: this.getSelectedMarkerIconSrc(), iconSize: selectedMarkerSize});
     }
 
     setMarkersToValue(wrapper: INode) {
-        const selectedMarkers: Array<{ position: LatLngLiteral, meta: Map<string, any> }> = [];
+        const selectedMarkers: Array<{ position: L.LatLngLiteral, meta: Map<string, any> }> = [];
         this.markers.forEach(marker => {
             if (marker.getElement()) {
                 const active = JSON.parse(marker.getElement()!.getAttribute("data-active")!);
