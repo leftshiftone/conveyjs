49113e0dab4569ec49d7da5bdabbd50bd0c32e33
Manuel Wagner <manuelj.wagner@gmail.com>
2020-02-20 11:28:01 +0100

    changed basket creation

--- src/lib/support/InputContainer.ts
+++ src/lib/support/InputContainer.ts
@@ -1,7 +1,7 @@
 import {ChoiceContainer} from "../renderable/choice/ChoiceContainer";
 import {ChoiceAggregator} from "../renderable/submit/ChoiceAggregator";
 import {SubmitState} from "../renderable/submit/SubmitState";
-import node from "./node";
+import node, {INode} from "./node";
 import wrap from "./node";
 import {ChoiceAggregationResult} from "../renderable/submit/ChoiceAggregationResult";
 
@@ -129,19 +129,9 @@ export class InputContainer {
             }
 
             const basket = wrap(element).getParentByClass("lto-basket");
-            if(basket) {
+            if (basket) {
                 const basketName = this.getNormalOrDataAttribute(basket.unwrap(), "name");
-
-                if (basketName) {
-                    const contextBasket = attributes[basketName];
-                    if (contextBasket !== undefined) {
-                        let curBasketEntity = contextBasket[contextBasket.length-1];
-                        curBasketEntity !== undefined && curBasketEntity.basketEntity.filter((it:any) => Object.keys(it).find(k => k === name) !== undefined).length === 0 ?
-                            curBasketEntity.basketEntity.push({[name]:preparedValue}) : attributes[basketName].push({['basketEntity']: [{[name]:preparedValue}]});
-                    } else {
-                        attributes[basketName] = [{['basketEntity']: [{[name]:preparedValue}]}];
-                    }
-                }
+                basketName && this.createBasket(basketName, attributes, name, preparedValue);
             } else {
                 attributes[name] !== undefined ?
                     attributes[name].push(preparedValue) :
@@ -151,6 +141,21 @@ export class InputContainer {
 
         element.classList.remove("lto-error");
         return SubmitState.ALLOWED;
+    }
+
+    private static createBasket(basketName : string, attributes: Attr, name : string, value : string) {
+            const contextBasket = attributes[basketName];
+            if (contextBasket !== undefined) {
+                const curBasketItem = contextBasket[contextBasket.length - 1];
+
+                if (!curBasketItem.basketItem.hasOwnProperty(name)) {
+                    curBasketItem.basketItem[name] = value;
+                } else {
+                    contextBasket.push({['basketItem']: {[name]:value}});
+                }
+            } else {
+                attributes[basketName] = [{['basketItem']: {[name]:value}}];
+            }
     }
 
     private static getNormalOrDataAttribute(element: HTMLElement, name: string): string | null {
