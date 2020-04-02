891ce4dd7db8370e64b7f82921cb3e84666d0aba
oliver <oliver.woelbitsch@leftshift.one>
2020-04-02 11:51:35 +0200

    [GAIA-1149] fixed reconnection issues (unregister existing IBehaviours)

--- src/lib/behaviour/MouseBehaviour.ts
+++ src/lib/behaviour/MouseBehaviour.ts
@@ -1,5 +1,4 @@
 import {IBehaviour, IRenderer, ISpecification} from '../api';
-import {MqttConnection} from '../connection/MqttConnection';
 import {ChannelType} from '../support/ChannelType';
 import {Defaults} from '../support/Defaults';
 
@@ -7,43 +6,34 @@ import {Defaults} from '../support/Defaults';
  * IBehaviour implementation which listens for a mouse click event in order to publish
  * a text message to the outgoing text channel.
  */
-export class MouseBehaviour implements IBehaviour {
+export class MouseBehaviour extends IBehaviour {
 
-    private readonly target1: HTMLButtonElement;
     private readonly target2: HTMLTextAreaElement;
     private readonly renderer: IRenderer;
     private readonly callback: (() => void) | undefined;
 
     constructor(renderer: IRenderer, target1?: HTMLButtonElement, target2?: HTMLTextAreaElement, callback?: () => void) {
-        this.target1 = target1 || Defaults.invoker();
+        super(target1 || Defaults.invoker(), { type: 'click', handler: () => this.handler() });
         this.target2 = target2 || Defaults.textbox();
         this.renderer = renderer;
         this.callback = callback;
+        this.gateway = null;
     }
 
-    /**
-     * Adds an event listener to the invoker.
-     * The user is able to publish a message by
-     * clicking the invoker.
-     *
-     * @inheritDoc
-     */
-    public bind(gateway: MqttConnection): void {
-        this.target1.addEventListener("click", (() => {
-            const value = this.target2.value;
+    private handler() {
+        const value = this.target2.value;
 
-            if (value.replace(/^\s+|\s+$/g, "") !== "") {
-                gateway.publish(ChannelType.TEXT, {type: "text", text: value});
-                this.target2.value = "";
+        if (this.gateway && value.replace(/^\s+|\s+$/g, "") !== "") {
+            this.gateway.publish(ChannelType.TEXT, {type: "text", text: value});
+            this.target2.value = "";
 
-                const payload = {type:"text", text: value, position:"right"} as ISpecification;
-                this.renderer.render(payload).forEach(e => this.renderer.appendContent(e));
+            const payload = {type:"text", text: value, position:"right"} as ISpecification;
+            this.renderer.render(payload).forEach(e => this.renderer.appendContent(e));
 
-                if (this.callback !== undefined) {
-                    this.callback();
-                }
+            if (this.callback !== undefined) {
+                this.callback();
             }
-        }));
+        }
     }
 
 }
