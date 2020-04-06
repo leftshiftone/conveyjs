377ff0b8e5c37c682667094aceac2f240451adca
David Reindl <david.reindl@leftshift.one>
2020-04-06 13:23:51 +0200

    junit-tests for renderables item, label and link

---
+++ src/lib/renderable/item/__tests__/item.test.ts
@@ -0,0 +1,22 @@
+import {Item} from "../index";
+import {ContentCentricRenderer} from "../../../renderer/ContentCentricRenderer";
+
+describe("ItemTest", () => {
+    it("beforeAll", () => {
+        document.body.innerHTML = `<div class="lto-gaia lto-content lto-suggest"/>`
+    });
+    it("render", () => {
+        const item = new Item({type: "Item"});
+        const element = item.render(new ContentCentricRenderer(), true);
+        expect(element.tagName).toBe("LI");
+        expect(element.className).toBe("lto-item lto-left");
+        expect(element.childElementCount).toBe(0);
+    });
+    it("renderMultipleItems", () => {
+        const item = new Item({elements: [{type: "Item"},{type: "Item"}] , type: "Item"});
+        const element = item.render(new ContentCentricRenderer(), true);
+        expect(element.tagName).toBe("LI");
+        expect(element.className).toBe("lto-item lto-left");
+        expect(element.childElementCount).toBe(2);
+    });
+});
--- src/lib/renderable/items/index.ts
+++ src/lib/renderable/items/index.ts
@@ -26,7 +26,7 @@ export class Items implements IRenderable, IStackeable {
      */
     public render(renderer: IRenderer, isNested: boolean): HTMLElement {
         const items = node(this.spec.ordered ? "ol" : "ul");
-        items.setId(this.spec.id)
+        items.setId(this.spec.id);
         items.addClasses("lto-items", "lto-left");
         this.spec.class !== undefined ? items.addClasses(this.spec.class) : () => {};
         (this.spec.elements || []).map(e => renderer.render(e, this))
