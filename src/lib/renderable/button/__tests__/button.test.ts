f1f258b16ede7068af82162d049ca3a8d2d9b9e2
Patrick Arbeiter <patrick.arbeiter@leftshift.one>
2020-04-07 13:07:46 +0200

    added test to button.test.ts

--- src/lib/renderable/button/__tests__/button.test.ts
+++ src/lib/renderable/button/__tests__/button.test.ts
@@ -6,7 +6,7 @@ describe("Button", () => {
     it("beforeAll", () => {
         Renderables.register("button", Button);
     });
-    it("render", () => {
+    it("render <button><label>Test Button</label></button>", () => {
         const mock = document.createElement("div");
         const mockCallBack = jest.fn();
         const form = new Form({
@@ -23,6 +23,21 @@ describe("Button", () => {
         const label = button.querySelector(".lto-label") as HTMLElement;
         expect(label).not.toBeNull();
         expect(label.innerText).toContain('Test Button');
+        button.click();
+        expect(mockCallBack).toHaveBeenCalledTimes(1);
+    });
+    it("render <button>Test Button</button>", () => {
+        const mock = document.createElement("div");
+        const mockCallBack = jest.fn();
+        const form = new Form({
+            type: "form", name: "form", elements: [{
+                type: "button", name: "button", text: "Test Button", elements: [ ]
+            }]
+        }).render(new ClassicRenderer(mock, mock), false);
+        const button = form.querySelector(".lto-button") as HTMLElement;
+        button.onclick = mockCallBack;
+        expect(button).not.toBeNull();
+        expect(button.innerText).toContain('Test Button');
         button.click();
         expect(mockCallBack).toHaveBeenCalledTimes(1);
     });
