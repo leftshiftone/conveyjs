490a49c6a0c1641eceaf0aa95e0eae5abcdc5e3e
David Reindl <31731749+Nerminas@users.noreply.github.com>
2020-04-09 13:35:20 +0200

    junit-tests for renderables item, label and link (#22)
    * junit-tests for renderables item, label and link
    
    * cleanup
    
    * fixed unit-test
    
    Co-authored-by: David Reindl <david.reindl@leftshift.one>
    Co-authored-by: malleran <52961928+malleran@users.noreply.github.com>

---
+++ src/lib/renderable/label/__tests__/label.test.ts
@@ -0,0 +1,23 @@
+import {Label} from "../index";
+import {ContentCentricRenderer} from "../../../renderer/ContentCentricRenderer";
+
+describe("LabelTest", () => {
+    it("beforeAll", () => {
+        document.body.innerHTML = `<div class="lto-gaia lto-content lto-suggest"/>`
+    });
+
+    it("renderNested", () => {
+        const label = new Label({type: "Label"});
+        const element = label.render(new ContentCentricRenderer(), true);
+        expect(element.tagName).toBe("DIV");
+        expect(element.className).toBe("lto-label lto-left lto-nested");
+    });
+
+    it("renderNotNested", () => {
+        const label = new Label({position: "right", type: "Label"});
+        const container = label.render(new ContentCentricRenderer(), false);
+        expect(container.tagName).toBe("DIV");
+        expect(container.childElementCount).toBe(2);
+        expect(container.children[1].className).toBe("lto-label lto-right")
+    });
+});
