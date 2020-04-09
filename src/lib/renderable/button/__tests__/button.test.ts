74e1cfe5e6de1aeffeea216b323f9594b28d1f38
Patrick <41366396+ptrcc@users.noreply.github.com>
2020-04-09 16:24:04 +0200

    Feature/gaia 1401 (#24)
    * modified button
    
    * added button.test.ts
    
    * added test to button.test.ts
    
    Co-authored-by: aemaem <michael.mair@leftshift.one>

---
+++ src/lib/renderable/button/__tests__/button.test.ts
@@ -0,0 +1,44 @@
+import {ClassicRenderer} from "../../../renderer/ClassicRenderer";
+import Renderables from "../../Renderables";
+import {Form, Button} from "../../../../std";
+
+describe("Button", () => {
+    it("beforeAll", () => {
+        Renderables.register("button", Button);
+    });
+    it("render <button><label>Test Button</label></button>", () => {
+        const mock = document.createElement("div");
+        const mockCallBack = jest.fn();
+        const form = new Form({
+            type: "form", name: "form", elements: [{
+                type: "button", name: "button", elements: [{
+                    type: "label", text: "Test Button"
+                }]
+            },
+            ]
+        }).render(new ClassicRenderer(mock, mock), false);
+        const button = form.querySelector(".lto-button") as HTMLElement;
+        button.onclick = mockCallBack;
+        expect(button).not.toBeNull();
+        const label = button.querySelector(".lto-label") as HTMLElement;
+        expect(label).not.toBeNull();
+        expect(label.innerText).toContain('Test Button');
+        button.click();
+        expect(mockCallBack).toHaveBeenCalledTimes(1);
+    });
+    it("render <button>Test Button</button>", () => {
+        const mock = document.createElement("div");
+        const mockCallBack = jest.fn();
+        const form = new Form({
+            type: "form", name: "form", elements: [{
+                type: "button", name: "button", text: "Test Button", elements: [ ]
+            }]
+        }).render(new ClassicRenderer(mock, mock), false);
+        const button = form.querySelector(".lto-button") as HTMLElement;
+        button.onclick = mockCallBack;
+        expect(button).not.toBeNull();
+        expect(button.innerText).toContain('Test Button');
+        button.click();
+        expect(mockCallBack).toHaveBeenCalledTimes(1);
+    });
+});
