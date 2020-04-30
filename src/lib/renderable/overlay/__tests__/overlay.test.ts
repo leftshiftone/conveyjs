83e5ce889cad63d8951bf185fecd27e33604e26d
David Reindl <david.reindl@leftshift.one>
2020-04-30 12:01:48 +0200

    cleanup overlay.test

--- src/lib/renderable/overlay/__tests__/overlay.test.ts
+++ src/lib/renderable/overlay/__tests__/overlay.test.ts
@@ -1,30 +1,24 @@
-import {Overlay} from "../index";
-import {ContentCentricRenderer} from "../../../renderer/ContentCentricRenderer";
+import {ClassicRenderer} from "../../../renderer/ClassicRenderer";
+import Renderables from "../../Renderables";
 import {fireEvent} from "@testing-library/dom"
 import {Item} from "../../item";
+import {Overlay} from "../index";
 
 describe("OverlayTest", () => {
     it("beforeAll", () => {
-        document.body.innerHTML = `<div class="lto-gaia lto-content lto-suggest"/>`
+        Renderables.register("overlay", Overlay);
+        Renderables.register("item", Item);
     });
 
-    it("render", () => {
-        const overlay = new Overlay(new Item({type:"item"}));
-        const element = overlay.render(new ContentCentricRenderer(), true);
-        expect(element.tagName).toBe("DIV");
-        expect(element.className).toBe("lto-overlay");
-
-    })
-
-    it("removeChild", () => {
-        const parentDiv = document.createElement("div");
-        const overlay = new Overlay(new Item({elements: [{type: "Item"}] , type: "Item"}));
-        const overlayElement = overlay.render(new ContentCentricRenderer(), true);
+    it("renderAndRemoveChild", () => {
+        const mock = document.createElement("div");
+        const overlay = new Overlay(new Item({elements: [{type: "Item"}], type: "Item"}));
+        const overlayElement = overlay.render(new ClassicRenderer(mock, mock), true);
         const itemElement = overlayElement.children[0].children[1];
         const buttonElement = overlayElement.children[0].children[0];
         expect(itemElement.tagName).toBe("LI");
         expect(buttonElement.tagName).toBe("BUTTON");
-        parentDiv.appendChild(overlayElement);
+        mock.appendChild(overlayElement);
         expect(overlayElement.parentElement).toBeTruthy();
         fireEvent.click(buttonElement, {button: 0});
         expect(overlayElement.parentElement).toBeFalsy()
