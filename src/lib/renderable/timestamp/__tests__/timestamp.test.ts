8e961dd18dda678cbd3c60b402f88974e683f1cd
malleran <andreas.maller@leftshift.one>
2020-05-08 09:37:59 +0200

    [LSO] (add tests for timestamp)

---
+++ src/lib/renderable/timestamp/__tests__/timestamp.test.ts
@@ -0,0 +1,18 @@
+import Renderables from "../../Renderables";
+import {Timestamp} from "../index";
+
+describe("Upload", () => {
+    it("beforeAll", () => {
+        Renderables.register("timestamp", Timestamp);
+    });
+
+    it("render", () => {
+        const timeStamp = Timestamp.render();
+
+        expect(timeStamp).not.toBeNull();
+        const element = timeStamp as HTMLElement;
+        expect(element.classList).toContain("lto-timestamp");
+        expect(element.firstChild).not.toBeNull();
+        expect(element!!.firstChild!!.nodeType).toBe(3);
+    });
+});
