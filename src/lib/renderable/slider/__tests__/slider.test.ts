a5a95c7a324a4e956e4b7ca4c879cc42b1828bb6
malleran <andreas.maller@leftshift.one>
2020-05-08 10:34:21 +0200

    [LSO] (add tests for slider)

--- src/lib/renderable/slider/__tests__/slider.test.ts
+++ src/lib/renderable/slider/__tests__/slider.test.ts
@@ -1,5 +1,6 @@
 import Renderables from "../../Renderables";
 import {Slider, ClassicRenderer} from "../../../../std";
+import {fireEvent} from "@testing-library/dom"
 
 describe("Slider", () => {
     it("beforeAll", () => {
@@ -22,6 +23,16 @@ describe("Slider", () => {
         expect(attributes.getNamedItem("type")!!.value).toBe("range");
         expect(attributes.getNamedItem("min")!!.value).toBe("0");
         expect(attributes.getNamedItem("max")!!.value).toBe("3");
+        expect(attributes.getNamedItem("value")!!.value).toBe("test2");
+
+        const nextButton = slider.querySelector(".lto-slider-next") as HTMLAnchorElement;
+        expect(nextButton).not.toBeNull();
+        fireEvent.click(nextButton);
+        expect(attributes.getNamedItem("value")!!.value).toBe("test3");
+
+        const prevButton = slider.querySelector(".lto-slider-prev") as HTMLAnchorElement;
+        expect(prevButton).not.toBeNull();
+        fireEvent.click(prevButton);
         expect(attributes.getNamedItem("value")!!.value).toBe("test2");
     });
 
