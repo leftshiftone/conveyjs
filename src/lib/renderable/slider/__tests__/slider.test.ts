18e8fe196e0bd5f139c76b948f34ea5a0b00fd47
malleran <52961928+malleran@users.noreply.github.com>
2020-05-08 11:21:14 +0200

    Feature/new tests (#31)
    * [LO-164] (Geo location via browser)
    
    * [LSO] (add tests for overlays)
    
    * [LSO] (add tests for image)
    
    * [LSO] (add tests for timestamp)
    
    * [LSO] (add tests for slider)
    
    * [LSO] (add tests for slider)

---
+++ src/lib/renderable/slider/__tests__/slider.test.ts
@@ -0,0 +1,40 @@
+import Renderables from "../../Renderables";
+import {Slider, ClassicRenderer} from "../../../../std";
+import {fireEvent} from "@testing-library/dom"
+
+describe("Slider", () => {
+    it("beforeAll", () => {
+        Renderables.register("slider", Slider);
+    });
+
+    it("render", () => {
+        const mock = document.createElement("div");
+        const slider = new Slider(  {
+            type: "slider",
+            name: "slider",
+            values: ["test1", "test2", "test3", "test4"],
+            value: "1",
+            step: "1"
+        }).render(new ClassicRenderer(mock, mock), false);
+
+        const sliderElement = slider.querySelector(".lto-slider") as HTMLInputElement;
+        expect(sliderElement).not.toBeNull();
+        const attributes = sliderElement.attributes;
+        expect(attributes.getNamedItem("type")!!.value).toBe("range");
+        expect(attributes.getNamedItem("min")!!.value).toBe("0");
+        expect(attributes.getNamedItem("max")!!.value).toBe("3");
+        expect(attributes.getNamedItem("value")!!.value).toBe("test2");
+
+        const nextButton = slider.querySelector(".lto-slider-next") as HTMLAnchorElement;
+        expect(nextButton).not.toBeNull();
+        fireEvent.click(nextButton);
+        expect(attributes.getNamedItem("value")!!.value).toBe("test3");
+
+        const prevButton = slider.querySelector(".lto-slider-prev") as HTMLAnchorElement;
+        expect(prevButton).not.toBeNull();
+        fireEvent.click(prevButton);
+        expect(attributes.getNamedItem("value")!!.value).toBe("test2");
+    });
+
+
+});
