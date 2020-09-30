e0a8fcd79e3fa3449bf90dbe44a81ef891f660c0
David Reindl <david.reindl@gmail.com>
2020-09-30 16:04:20 +0200

    [LO-322] possible fix for window is not defined error

--- src/lib/audio/player/Base64ToArrayBufferConverter.ts
+++ src/lib/audio/player/Base64ToArrayBufferConverter.ts
@@ -5,12 +5,15 @@ export class Base64ToArrayBufferConverter {
      * @returns {ArrayBufferLike}
      */
     public static base64ToArrayBuffer(base64: string) : ArrayBufferLike {
-        const binaryString =  window.atob(base64);
-        const len = binaryString.length;
-        const bytes = new Uint8Array(len);
-        for (let i = 0; i < len; i++)        {
-            bytes[i] = binaryString.charCodeAt(i);
+        if(typeof window !== "undefined") {
+            const binaryString =  window.atob(base64);
+            const len = binaryString.length;
+            const bytes = new Uint8Array(len);
+            for (let i = 0; i < len; i++)        {
+                bytes[i] = binaryString.charCodeAt(i);
+            }
+            return bytes.buffer;
         }
-        return bytes.buffer;
+        return new Uint8Array
     }
 }
