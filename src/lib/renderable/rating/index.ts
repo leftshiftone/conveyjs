f2eab0e9605defe69caf05c36ad3637b11082e1a
Lorenz Leitner <lorenz.leitner@leftshift.one>
2020-10-05 12:31:20 +0200

    [GAIA-1680] Simplify button handling

--- src/lib/renderable/rating/index.ts
+++ src/lib/renderable/rating/index.ts
@@ -1,6 +1,6 @@
 import {IRenderer, ISpecification, IRenderable, IStackeable} from '../../api';
 import Renderables from '../Renderables';
-import node from "../../support/node";
+import node, {INode} from "../../support/node";
 import {Specification} from "../../support/Specification";
 import {MessageType} from "../../support/MessageType";
 import {EventType} from "../../event/EventType";
@@ -20,6 +20,11 @@ export class Rating implements IRenderable, IStackeable {
 
     public static readonly TYPE = "rating";
     private readonly spec: ISpecification;
+    private readonly ratingContainer: INode;
+    private readonly commentForm: INode;
+    private readonly buttonLike: INode;
+    private readonly buttonDislike: INode;
+    private likeButtons: Map<ButtonType, INode>;
 
     constructor(message: ISpecification) {
         this.spec = message;
@@ -27,6 +32,14 @@ export class Rating implements IRenderable, IStackeable {
         this.setSpecPropertyIfNotDefined("name", "rating");
         this.setSpecPropertyIfNotDefined("position", "left");
         this.setSpecPropertyIfNotDefined("timestamp", new Date().getTime().toString());
+
+        this.ratingContainer = node("div");
+        this.commentForm = node("form");
+        this.buttonLike = node("button");
+        this.buttonDislike = node("button");
+        this.likeButtons = new Map<ButtonType, INode>();
+        this.likeButtons.set(ButtonType.LIKE, this.buttonLike);
+        this.likeButtons.set(ButtonType.DISLIKE, this.buttonDislike);
     }
 
     /**
@@ -39,45 +52,35 @@ export class Rating implements IRenderable, IStackeable {
         const attributes = {name, value};
 
         // Div for both buttons
-        const rating = node("div");
-        new Specification(this.spec).initNode(rating, "lto-rating");
-        rating.addAttributes({type: Rating.TYPE});
+        new Specification(this.spec).initNode(this.ratingContainer, "lto-rating");
+        this.ratingContainer.addAttributes({type: Rating.TYPE});
 
         // Add like and dislike buttons
-        const buttonLike = node("button");
-        buttonLike.innerText('👍');
-        rating.appendChild(buttonLike);
-        const buttonDislike = node("button");
-        rating.appendChild(buttonDislike);
-        buttonDislike.innerText('👎');
+        this.buttonLike.innerText('👍');
+        this.ratingContainer.appendChild(this.buttonLike);
+        this.ratingContainer.appendChild(this.buttonDislike);
+        this.buttonDislike.innerText('👎');
 
         // Prepare comment form
-        const commentForm = node("form");
-        commentForm.appendChild(node("input").addAttributes({type: "text"}));
+        this.commentForm.appendChild(node("input").addAttributes({type: "text"}));
         const submitButton = node("button").innerText("Send rating");
-        commentForm.appendChild(submitButton);
+        this.commentForm.appendChild(submitButton);
 
-        buttonLike.onClick((ev: MouseEvent) => {
+        this.buttonLike.onClick((ev: MouseEvent) => {
             ev.preventDefault();
-            console.log("Clicked buttonLike");
-            rating.appendChild(commentForm);
-            buttonDislike.addAttributes({disabled: ""});
-            buttonLike.addDataAttributes({clicked: true});
+            this.likeButtonOnClick(ButtonType.LIKE);
         });
 
-        buttonDislike.onClick((ev: MouseEvent) => {
+        this.buttonDislike.onClick((ev: MouseEvent) => {
             ev.preventDefault();
-            console.log("Clicked buttonDislike");
-            rating.appendChild(commentForm);
-            buttonLike.addAttributes({disabled: ""});
-            buttonDislike.addDataAttributes({clicked: true});
+            this.likeButtonOnClick(ButtonType.DISLIKE);
         });
 
         submitButton.onClick((ev: MouseEvent) => {
             let score;
-            if (buttonLike.getDataAttribute("clicked")) {
+            if (this.buttonLike.getDataAttribute("clicked")) {
                 score = "1";
-            } else if (buttonDislike.getDataAttribute("clicked")) {
+            } else if (this.buttonDislike.getDataAttribute("clicked")) {
                 score = "0";
             }
             const payload = {text, name, score};
@@ -86,12 +89,27 @@ export class Rating implements IRenderable, IStackeable {
             EventStream.emit(evType, {attributes, type, payload} as IEventPayload);
         });
 
-        return rating.unwrap();
+        return this.ratingContainer.unwrap();
     }
 
     private setSpecPropertyIfNotDefined(property: string, value: string) {
         this.spec[property] = (this.spec[property] === undefined ? value : this.spec[property]);
     }
+
+    private likeButtonOnClick(buttonType: ButtonType) {
+        this.ratingContainer.appendChild(this.commentForm);
+        // The other button
+        this.likeButtons.get((buttonType + 1) % ButtonType.__LENGTH)!.addAttributes({disabled: ""});
+        this.likeButtons.get((buttonType + 1) % ButtonType.__LENGTH)!.setStyle({cursor: "not-allowed"});
+        // The button that was clicked
+        this.likeButtons.get(buttonType)!.addDataAttributes({clicked: true});
+    }
 }
 
 Renderables.register("rating", Rating);
+
+enum ButtonType {
+    LIKE,
+    DISLIKE,
+    __LENGTH
+}
