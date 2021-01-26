34670bb3e11452ad5b2b0c54545f5baacf8f4c3b
Raphael Schluesselbauer <raphael.schluesselbauer@leftshift.one>
2021-01-26 11:34:59 +0100

    [GAIA-2427] Fix test

--- src/lib/renderer/__tests__/RatingDecorator.test.ts
+++ src/lib/renderer/__tests__/RatingDecorator.test.ts
@@ -6,7 +6,7 @@ import {NoopRating} from "../../renderable/rating/NoopRating";
 import each from "jest-each";
 import {RatingDecorator, RatingRenderStrategy} from "../decorator/RatingDecorator";
 import {ContentCentricRenderer} from "../ContentCentricRenderer";
-import {RatingTestSpecGenerator} from "./RatingTestSpecGenerator";
+import {ISpecification} from "../../api";
 
 
 describe("RatingDecorator test", () => {
@@ -98,4 +98,48 @@ describe("RatingDecorator test", () => {
     });
 });
 
+class RatingTestSpecGenerator {
+    static generate(withRatingMarkup: boolean, ratingEnabled: boolean) {
+        const content = [{
+            class: "some-class",
+            type: "block",
+            elements: [
+                {
+                    class: "uppercase bold",
+                    text: "Some very informative text",
+                    type: "label"
+                },
+                {
+                    text: "More very informative text",
+                    type: "label"
+                }
+            ]
+        }];
+
+        if (withRatingMarkup) {
+            return RatingTestSpecGenerator.wrapInContainer([{
+                type: "rating",
+                enabled: ratingEnabled,
+                elements: content
+            }]);
+        }
+
+        return RatingTestSpecGenerator.wrapInContainer(content);
+    }
+
+    private static wrapInContainer(elements: ISpecification[]) {
+        return {
+            type: "container",
+            qualifier: "prompt:next",
+            position: "left",
+            enriched: {
+                executionGroupId: "123-executionGroupId",
+                processId: "123-processId",
+                identityId: "123-identityId",
+                nodeId: "123-nodeId"
+            },
+            elements
+        } as ISpecification;
+    }
+}
 
