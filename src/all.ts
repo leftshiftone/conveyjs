849836b92d8d046573d6e59ff608a6eab9616203
rschluesselbauer <raphael.schluesselbauer@leftshift.one>
2021-03-31 11:37:34 +0200

    Feature/rating event payload (#79)
    * [INFRASTRUCTURE] Move rating event payload into factory
    
    * [INFRASTRUCTURE] Add enriched to ISpecification
    * add enriched to specification - it has been here implicitly already
    * add createFromSpecification method to ProcessNode and use in RatingDecorator
    * correctly use jest before all
    
    * [INFRASTRUCTURE] Use EventFactory in Rating
    
    * [INFRASTRUCTURE] Change any to object type

--- src/all.ts
+++ src/all.ts
@@ -88,6 +88,8 @@ import {Subscription} from './lib/connection/Subscription';
 import {InteractionSubscription} from "./lib/connection/InteractionSubscription";
 import {ConversationQueueType, MqttSensorQueue, QueueOptions, QueueHeader, QueueCallback} from "@leftshiftone/gaia-sdk/dist";
 import { FlexSearchIndex } from './lib/behaviour/autocomplete/index/FlexSearchIndex';
+import { EventFactory } from './lib/event/EventFactory';
+import { EventType } from './lib/event/EventType';
 export * from './lib/api';
 export {
     ConversationQueueType, MqttSensorQueue, QueueOptions, QueueHeader, QueueCallback,
@@ -113,6 +115,8 @@ export {
     Navigator,
     Renderables,
     Properties,
+    EventType,
+    EventFactory,
     EventStream,
     Block,
     Bold,
